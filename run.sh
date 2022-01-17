4eb724e223a632c1aeced1dfe73b1d4683814b70
hari <17010423+huevi@users.noreply.github.com>
2022-01-17 14:31:28 +0530

    add docker pull

--- run.sh
+++ run.sh
@@ -104,9 +104,10 @@ _set_hairpin() {
 _create_or_start_container() {
 	
 	if ! docker inspect $IMAGE:$TAG >/dev/null 2>&1; then
+		echo "docker pull '$IMAGE:$TAG' 
 		docker pull $IMAGE:$TAG 
-		echo "no image '$IMAGE:$TAG' found, did you forget to run 'make build'?"
-		exit 1
+		# echo "no image '$IMAGE:$TAG' found, did you forget to run 'make build'?"
+		# exit 1
 
 	elif docker inspect $CONTAINER >/dev/null 2>&1; then
 		echo "* starting container '$CONTAINER'"
